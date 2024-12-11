1bb6cb7649c6b2fb129bb537fd16bb74f95636a9
Marija Ilić <marija.cbsqg@gmail.com>
2024-12-11 10:42:29 +0100

    fixes

--- database/migrations/2024_12_09_145855_create_media_albums_table.php
+++ database/migrations/2024_12_09_145855_create_media_albums_table.php
@@ -14,10 +14,7 @@ return new class extends Migration
     {
         Schema::create('media_albums', function (Blueprint $table) {
             $table->uuid('id')->primary();
-
-            $table->uuid('user_id');
-            $table->foreign('user_id')->references('id')->on((new User())->getTable());
-
+            $table->foreignIdFor(User::class)->constrained();
             $table->timestamps();
         });
     }
