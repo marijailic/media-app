849e926bdf41f278374a47b89cc60e0f8bac4807
Marija Ilić <marija.cbsqg@gmail.com>
2024-12-12 16:01:47 +0100

    validacija

---
+++ app/Http/Requests/Api/StoreMediaAlbumRequest.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Http\Requests\Api;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class StoreMediaAlbumRequest extends FormRequest
+{
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
+     */
+    public function rules(): array
+    {
+        return [
+            'files.*' => ['required', 'file', 'mimes:jpeg,png,jpg,pdf,doc,docx,xls', 'max:2048'],
+        ];
+    }
+}
