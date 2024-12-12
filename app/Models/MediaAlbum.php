995070ace1e4f6f9b80b68684c0e7d984981c0e0
Marija Ilić <marija.cbsqg@gmail.com>
2024-12-12 10:12:45 +0100

    wip

--- app/Models/MediaAlbum.php
+++ app/Models/MediaAlbum.php
@@ -2,19 +2,28 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Concerns\HasUuids;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\Relations\HasMany;
+use Spatie\MediaLibrary\HasMedia;
+use Spatie\MediaLibrary\InteractsWithMedia;
 use Spatie\MediaLibrary\MediaCollections\Models\Media;
 
 
-class MediaAlbum extends Model
+class MediaAlbum extends Model implements HasMedia
 {
+    use InteractsWithMedia;
+    use HasFactory;
+    use HasUuids;
+
     protected $fillable = [
         'user_id',
     ];
 
-    public function media(): HasMany
+    public function user(): BelongsTo
     {
-        return $this->HasMany(Media::class);
+        return $this->belongsTo(User::class);
     }
 }
