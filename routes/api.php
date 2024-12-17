99a50526d3a25661e9954c9b44ed940ebc46731a
Marija Ilić <marija.cbsqg@gmail.com>
2024-12-17 12:04:27 +0100

    uklonjen update

--- routes/api.php
+++ routes/api.php
@@ -3,5 +3,5 @@
 use App\Http\Controllers\Api\MediaAlbumController;
 use Illuminate\Support\Facades\Route;
 
-Route::apiResource('media-album', MediaAlbumController::class);
+Route::apiResource('media-album', MediaAlbumController::class)->except(['update']);
 
