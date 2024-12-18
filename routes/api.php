9bcc7f498fd71f46aa37fc4671fcded5f552b093
Marija Ilić <marija.cbsqg@gmail.com>
2024-12-18 09:47:20 +0100

    uuid validacija na rutama

--- routes/api.php
+++ routes/api.php
@@ -12,12 +12,15 @@ Route::controller(MediaAlbumController::class)->prefix('media-album')->group(fun
         ->name('media-album.upload');
 
     Route::get('/{media_album}/media', 'getAlbumMedia')
+        ->whereUuid('media_album')
         ->name('media-album.media');
 
     Route::delete('/{media_album}', 'delete')
+        ->whereUuid('media_album')
         ->name('media-album.delete');
 
     Route::delete('/{media_album}/media/{media}', 'deleteMedia')
+        ->whereUuid('media_album')
         ->scopeBindings()
         ->name('media-album.delete-media');
 
